36cee5f1f14adbac0d03997f32855aa07a4a54ce
Isaac Pruett <121455801+Isaac-Pruett@users.noreply.github.com>
2026-01-16 12:42:01 -0800

    add encoding to submit fn

--- asgn_2/task_2.py
+++ asgn_2/task_2.py
@@ -1,12 +1,22 @@
 import urllib.parse
+from random import randbytes
+
+import numpy as np
 
 import block_ciphers
 
 
-def submit(text: str):
+def submit(text: str) -> bytes:
     begin = "userid=456;userdata="
     end = ";session-id=31337"
-    return urllib.parse.quote(begin + text + end)
+    url_text = urllib.parse.quote(begin + text + end)
+    plaintext = url_text.encode()
+
+    plaintext_np = np.frombuffer(plaintext, dtype=np.uint8)
+
+    key = randbytes(16)
+    IV = randbytes(16)
+    return block_ciphers.encrypt_cbc(key, plaintext_np, IV, 10).tobytes()
 
 
 def main():

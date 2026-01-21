946f3f4dd4c83a54f706e685783cd324671f0a33
Sami Marcos <ssmarcos@calpoly.edu>
2026-01-21 10:24:00 -0800

    hard-code p and g

--- asgn_3/diffie_hellman.py
+++ asgn_3/diffie_hellman.py
@@ -2,10 +2,16 @@ from Crypto.Cipher import AES
 from Crypto.Hash import HMAC, SHA256
 from Crypto.Random import get_random_bytes
 
+GLOBAL_MOD_P = 23
+GLOBAL_BASE_G = 5
+
 
 def main():
+    message = "Hello world"
     pass
 
 
 if __name__ == "__main__":
     main()
+
+

f3186401994dbf3bf238b4cf65b2ace37402839c
Isaac Pruett <121455801+Isaac-Pruett@users.noreply.github.com>
2026-01-26 10:22:37 -0800

    remove disallowed dependencies. add hamming fn

--- asgn_4/main.py
+++ asgn_4/main.py
@@ -1,16 +1,26 @@
+from ast import Return
+
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from Crypto.Util.Padding import pad, unpad
-import textdistance
+
+
+def hamming_dist(str1: str, str2: str) -> int:
+    count = 0
+    for c1, c2 in zip(str1, str2):
+        if c1 != c2:
+            count += 1
+    return count
+
 
 def main():
     ##### TASK 1 #####
 
-    # a) . Write a program that uses SHA256 to hash arbitrary inputs and print 
+    # a) . Write a program that uses SHA256 to hash arbitrary inputs and print
     # the resulting digests to the screen in hexadecimal format.
 
-    #user_in = input("Enter input: ")
-    
+    # user_in = input("Enter input: ")
+
     str1 = "Hello from asgn-4!"
     print(f"str1: {str1}")
     str1_k = SHA256.new()
@@ -19,7 +29,7 @@ def main():
 
     print(f"    str1 digest: {str1_k_bytes}")
 
-    #different by one bit (lowercase h)
+    # different by one bit (lowercase h)
     str2 = "hello from asgn-4!"
     print(f"str2: {str2}")
     str2_k = SHA256.new()
@@ -28,11 +38,13 @@ def main():
 
     print(f"    str2 digest: {str2_k_bytes}")
 
-    # B) hash two strings (of any length) whose Hamming distance is exactly 
+    # B) hash two strings (of any length) whose Hamming distance is exactly
     # 1 bit (i.e. differ in only 1 bit). Repeat this a few times
 
     ham_dist = textdistance.hamming.distance(str1, str2)
     print(f"Hamming distance: {ham_dist} == 1 ? {ham_dist == 1}")
-    #https://www.geeksforgeeks.org/dsa/hamming-distance-two-strings/
+    # https://www.geeksforgeeks.org/dsa/hamming-distance-two-strings/
+
+
 if __name__ == "__main__":
     main()
